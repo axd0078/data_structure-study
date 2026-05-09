3d08beb7b94dfc571be07ddad4e570de52547567
axd0078 <lana83155ana@gmail.com>
2026-05-09 23:33:52 +0800

    编程：补CSP07 08题

--- programming/test/csp/7/1.cpp
+++ programming/test/csp/7/1.cpp
@@ -4,5 +4,27 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    if(n<=2){
+        cout << "0" << endl;
+        return 0;
+    }
+    int pre,now,next;
+    cin >> pre >> now >> next;
+    n -= 3;
+    int ans = 0;
+    if((pre>=now && now >=next) || (pre<=now && now<= next));
+    else ans++;
+    while(n--){
+        int x;
+        cin >> x;
+        pre = now;
+        now = next;
+        next = x;
+        if((pre>=now && now >=next) || (pre<=now && now<= next));
+        else ans++;
+    }
+    cout << ans << endl;
     return 0;
 }
--- programming/test/csp/8/1.cpp
+++ programming/test/csp/8/1.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int pre,now;
+    cin >> pre >> now;
+    int max = abs(now-pre);
+    n -= 2;
+    while(n--){
+        pre = now;
+        cin >> now;
+        if(abs(now-pre) > max)
+            max = abs(now-pre);
+    }
+    cout << max << endl;
     return 0;
-}
+}//波动一定是正数
