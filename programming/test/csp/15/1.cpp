dd057c237c5e3d60e892fd5a7a96886b3567cbab
axd0078 <lana83155ana@gmail.com>
2026-05-19 09:56:42 +0800

    更新计划并完成部分题目

--- programming/test/csp/15/1.cpp
+++ programming/test/csp/15/1.cpp
@@ -4,5 +4,27 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int r,y,g;
+    cin >> r >> y >> g;
+    int n;
+    cin >> n;
+    int time = 0;
+    while(n--){
+        int k , t;
+        cin >> k >> t;
+        if(k==0){
+            time += t;
+        }
+        else if(k==1){
+            time += t;
+        }
+        else if(k==2){
+            time += (t + r);
+        }
+        else if(k==3){
+            continue;
+        }
+    }
+    cout << time << endl;
     return 0;
 }
--- programming/test/csp/16/1.cpp
+++ programming/test/csp/16/1.cpp
@@ -4,5 +4,50 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int temp = n;
+    int max = 0;
+    int min = INT_MAX;
+    int a,b,num1,num2;
+    if(n%2==1){
+        a = n/2+1;
+        b = 0;
+    }
+    else{
+        a = n/2;
+        b = n/2+1;
+    }
+    int i = 0;
+    while(temp--){
+        i++;
+        int num;
+        cin >> num;
+        if(num > max){
+            max = num;
+        }
+        if(num < min){
+            min = num;
+        }
+        if(i==a){
+            num1 = num;
+        }
+        if(i==b){
+            num2 = num;
+        }
+    }
+    cout << max << " ";
+    int ans;
+    if(n%2==1){
+        cout << num1 << " ";
+    }
+    else{
+        ans = num1+num2;
+        if((num1+num2)%2==1)
+            cout << fixed << setprecision(1) << ans/2.0 << " ";
+        else
+            cout << ans/2 << " ";
+    }
+    cout << min << endl;
     return 0;
 }
