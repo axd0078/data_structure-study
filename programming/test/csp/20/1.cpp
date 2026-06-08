dcf731128c0d479567cf2ef218ed3a97a15db579
axd0078 <lana83155ana@gmail.com>
2026-06-08 11:12:39 +0800

    完成20-1 21-1

--- programming/test/csp/20/1.cpp
+++ programming/test/csp/20/1.cpp
@@ -4,5 +4,28 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n,X,Y;
+    cin >> n >> X >> Y;
+    vector<pair<int,int>> ans;
+    int num = 1;
+    while(num<=n){
+        int x,y;
+        cin >> x >> y;
+        int distance = pow(X-x,2) + pow(Y-y,2);
+        ans.push_back({num,distance});
+        sort(ans.begin(),ans.end(),[](const pair<int,int>& a,const pair<int,int>& b){
+            if(a.second == b.second){
+                return a.first < b.first;
+            }
+            return a.second < b.second;
+        });
+        if(ans.size() > 3){
+            ans.pop_back();
+        }
+        num++;
+    }
+    for(int i=0;i<3;i++){
+        cout << ans[i].first << endl;
+    }
     return 0;
 }
--- programming/test/csp/21/1.cpp
+++ programming/test/csp/21/1.cpp
@@ -4,5 +4,14 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n;
+    cin >> n;
+    int ans = 0;
+    while(n--){
+        int w,s;
+        cin >> w >> s;
+        ans += w*s;
+    }
+    cout << max(ans,0) << endl;
     return 0;
 }
